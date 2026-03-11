f857bd2c3a7560406bf56ceb2bf5344761b1fc06
Sharero <yaroslav.sumin@mail.ru>
2026-03-11 22:48:30 +0700

    Добавил возможность вводить студентов с разных файлов. Оптимизировал код.

--- src/main/java/org/example/packageReadFile/ReadFileClass.java
+++ src/main/java/org/example/packageReadFile/ReadFileClass.java
@@ -1,73 +1,30 @@
 package org.example.packageReadFile;
 
 import org.example.collection.StudentsList;
-import org.example.menu.MenuConstructorClass;
-import org.example.model.Student;
-import org.example.packageInterface.FileProcessor;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+public class ReadFileClass {
 
-public class ReadFileClass extends FileManager {
-    private FileManager fileManager;
-    public ReadFileClass(){
-        fileManager = new FileManager();
-    }
-    //старая версия
-    public static void readFile() throws IOException {
+    private final FileManager fileManager = new FileManager();
 
-        List<Student> student = new ArrayList<>();
+    public StudentsList readFile(String fileName, int count) {
 
-        System.out.println("Введите имя файла для загрузки данных: ");
-        Scanner scanner = new Scanner(System.in);
-        String fileName = scanner.nextLine();
+        System.out.println("Количество студентов для считывания: " + count);
 
-        FileProcessor processor = new FileManager();
-        processor.processDeleteEmptyRemoverInterface(fileName);
-        processor.processReadFileInterface(student, fileName);
+        StudentsList result = new StudentsList();
 
-        while (true) {
+        StudentsList buffer = fileManager.readDataFromFile(fileName);
 
-            MenuConstructorClass menuSort = new MenuConstructorClass();
-            menuSort.sortMenu(student);
-
-            MenuConstructorClass menuConstructorClass = new MenuConstructorClass();
-            menuConstructorClass.saveFile();
-
-            MenuConstructorClass menuMain = new MenuConstructorClass();
-            menuMain.mainMenu();
+        if (count > buffer.size()) {
+            System.out.println("В файле недостаточно данных.");
+            return buffer;
         }
-    }
 
-    //имя файла, из которого считываем данные
-    private String fileName = "students.txt";
-    //список студентов, считанных из файла
-    private StudentsList students = new StudentsList();
-    //метод для считывания файла
-    //count - количество данных в массиве данные (по заданию пользователь имеет возможность ограничить)
-    public StudentsList readFile(int count){
-        System.out.println("Количество студентов для считывания из файла: "+count);
-        String fileName = "students.txt";
-        try {
-            var buf = fileManager.readDataFromFile(fileName);
-            if(count>buf.size()){
-                System.out.println("В файле недостаточно данных.");
-                students = buf;
-            }else{
-                buf.stream().limit(count).forEach(student -> students.add(student));
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.exit(1);
+        for (int i = 0; i < count; i++) {
+            result.add(buffer.get(i));
         }
-        students.forEach(System.out::println);
-        return students;
+
+        result.forEach(System.out::println);
+
+        return result;
     }
 }
