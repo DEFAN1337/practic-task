49f45fc9079a8f56605691f2c4d75171c3db7cd7
Sharero <yaroslav.sumin@mail.ru>
2026-03-11 22:50:01 +0700

    Сильно сократил код. Вынес блоки кода в функции. Свел всю логику с меню в класс для меню, а не разбросанно по разным классам.

--- src/main/java/org/example/menu/MenuConstructorClass.java
+++ src/main/java/org/example/menu/MenuConstructorClass.java
@@ -1,298 +1,238 @@
 package org.example.menu;
 
 import org.example.collection.StudentsList;
-import org.example.model.Student;
-import org.example.packageRandomWrite.*;
-import org.example.packageInterface.*;
+import org.example.packageRandomWrite.RandomFileClass;
+import org.example.packageRandomWrite.WriteFileClass;
 import org.example.packageReadFile.FileManager;
 import org.example.packageReadFile.ReadFileClass;
-import org.example.packageSort.SortedClass;
 import org.example.thread.FindThreadClass;
 import org.example.thread.TypeFindData;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MenuConstructorClass {
-    private final Scanner scanner;
-    private StudentsList students;
-    private FileManager fileManager;
+    private final Scanner scanner = new Scanner(System.in);
 
-    public MenuConstructorClass() {
-        this.scanner = new Scanner(System.in);
-        this.students = new StudentsList();
-        this.fileManager = new FileManager();
-    }
+    private final FileManager fileManager = new FileManager();
 
-    public void selectMainMenu() {
-        while (true) {
-            System.out.println("\nМеню 'Выбор ввода данных'");
-            System.out.println("\nСписок команд");
-            System.out.println("'manual_input' - ввод данных вручную");
-            System.out.println("'random_input' - ввод данных случайно");
-            System.out.println("'file_input' - ввод данных из файла");
-            System.out.println("'quit' - закрыть приложение");
+    private StudentsList studentsList = new StudentsList();
 
-            // если уже считали какие-то данные
-            if (!students.isEmpty()) {
-                System.out.println("'sort_menu' - вернуться в меню 'Сортировка данных'");
-                System.out.println("'find' - перейти в меню 'Поиск данных'");
+    public void start() {
 
-                // если данные отсортированы, то можем их сохранить в файл
-                if (students.isSorted()) {
-                    saveFile();
-                }
-            }
+        boolean running = true;
 
-            System.out.print("Введите команду: ");
-            String menu = scanner.nextLine();
-            checkCommand(menu);
-        }
-    }
+        while (running) {
 
-    public void mainMenu() throws IOException {
-        System.out.println("\nСписок команд");
-        System.out.println("'main_menu' - вернуться в меню 'Выбор ввода данных'");
-        System.out.println("'sort_menu' - вернуться в меню 'Сортировка данных'");
-        System.out.println("'quit' - закрыть приложение");
-        System.out.print("Введите команду: ");
+            printMainMenu();
+            String command = scanner.nextLine();
 
-        String menu = scanner.nextLine();
+            switch (command) {
 
-        switch (menu) {
-            case "main_menu":
-                selectMainMenu();
-                break;
-            case "sort_menu":
-                // sortMenu();
-                break;
-            case "quit":
-                System.exit(0);
-                break;
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...");
-                mainMenu();
-        }
-    }
-
-    public void sortMenu(List<Student> student) {
-        System.out.println("\nМеню 'Сортировка данных'");
-        System.out.println("\nСписок команд");
-        System.out.println("'sort_name' - отсортировать по имени");
-        System.out.println("'sort_grade' - отсортировать по оценочному баллу");
-        System.out.println("'sort_gradebook' - отсортировать по номеру зачетной книжки");
-        System.out.println("'quit' - закрыть приложение");
-        System.out.print("Введите команду: ");
-
-        String menu = scanner.nextLine();
-        SortProcessor sorted = new SortedClass();
-
-        switch (menu) {
-            case "sort_name":
-                students.sortByName();
-                break;
-            case "sort_grade":
-                students.sortByGrade();
-                break;
-            case "sort_gradebook":
-                students.sortByNumberGradebook();
-                break;
-            case "quit":
-                System.exit(0);
-                break;
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...\n");
-                sortMenu(student);
-        }
-    }
-
-    public void writeMenu(List<Student> student) throws IOException {
-        System.out.println("\nСписок команд");
-        System.out.println("'continue' - продолжить ввод данных");
-        System.out.println("'sort_menu' - открыть меню 'Сортировка данных'");
-        System.out.println("'quit' - закрыть приложение");
-        System.out.print("Введите команду: ");
-
-        String menu = scanner.nextLine();
-
-        switch (menu) {
-            case "continue":
-                System.out.println();
-                WriteFileClass writeFileClass = new WriteFileClass();
-                writeFileClass.writeFile();
-                break;
-            case "sort_menu":
-                sortMenu(student);
-                break;
-            case "quit":
-                System.exit(0);
-                break;
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...");
-                writeMenu(student);
-        }
-    }
-
-    public void saveFile() {
-        System.out.println("\nСохранить отсортированный файл?");
-        System.out.println("'yes' - да");
-        System.out.println("'no' - нет");
-        System.out.print("Введите команду: ");
-
-        String menu = scanner.nextLine();
-        FileProcessor processor = new FileManager();
-
-        switch (menu) {
-            case "yes":
-                System.out.println("Введите название файла:");
-                String fileName_str = scanner.nextLine();
-                fileManager.writeDataInFile(students, fileName_str);
-                students.clearSorted();
-                selectMainMenu();
-
-                System.out.println("Введите название файла:");
-                processor.processSaveFile(scanner.nextLine());
-                break;
-            case "no":
-                students.clearSorted();
-                selectMainMenu();
-                processor.processDeleteTempFile();
-                break;
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...");
-                saveFile();
-        }
-    }
-
-    // метод для проверки команд
-    public void checkCommand(String command) {
-        switch (command) {
-            // ручной ввод информации
-            case "manual_input":
-                System.out.println("\nВы выбрали ввод данных вручную\n");
-                System.out.println("Введите количество студентов:");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Введите количество студентов:");
-                    scanner.next();
-                }
-                int count_ = scanner.nextInt();
-                scanner.nextLine(); // очистка буфера после nextInt()
-                WriteFileClass writeFileClass = new WriteFileClass();
-                students = writeFileClass.writeDataStudent(count_);
-                break;
-
-            // генерация данных
-            case "random_input":
-                System.out.println("\nВы выбрали ввод данных случайно");
-                RandomFileClass randomFileClass = new RandomFileClass();
-                try {
-                    randomFileClass.randomFile();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-                break;
-
-            // считывание данных из файла
-            case "file_input":
-                System.out.println("\nВы выбрали ввод данных из файла");
-                ReadFileClass readFileClass = new ReadFileClass();
-                System.out.println("Введите количество студентов:");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Введите количество студентов:");
-                    scanner.next();
-                }
-                int count = scanner.nextInt();
-                scanner.nextLine(); // очистка буфера после nextInt()
-                students = readFileClass.readFile(count);
-                break;
-
-            // выход из программы
-            case "quit":
-                System.exit(0);
-                break;
-
-            // отображение меню для сортировки
-            case "sort_menu":
-                sortMenu(students);
-                break;
-
-            case "find":
-                findData();
-                break;
-
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...");
-                selectMainMenu();
-        }
-    }
-
-    // отображение меню для поиска данных
-    void findData() {
-        System.out.println("\nМеню 'Поиск данных'");
-        System.out.println("\nСписок команд");
-        System.out.println("'find_name' - поиск по имени");
-        System.out.println("'find_grade' - отсортировать по оценочному баллу");
-        System.out.println("'find_gradebook' - отсортировать по номеру зачетной книжки");
-        System.out.println("'quit' - закрыть приложение");
-        System.out.print("Введите команду: ");
-
-        String menu = scanner.nextLine();
-
-        switch (menu) {
-            case "find_name": {
-                System.out.println("Введите имя студента для поиска: ");
-                String name = scanner.nextLine();
-                while (!name.matches("^[а-яА-Я]+$")) {
-                    System.out.println("Введите имя студента для поиска: ");
-                    name = scanner.nextLine();
-                }
-                FindThreadClass thread = new FindThreadClass(TypeFindData.Name, students, name);
-                thread.startThread();
-                break;
-            }
-            case "find_grade": {
-                System.out.println("Введите оценку для поиска: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Введите оценку для поиска: ");
-                    scanner.next();
-                }
-                int grade = scanner.nextInt();
-                scanner.nextLine(); // очистка буфера после nextInt()
-
-                while (grade < 2 || grade > 5) {
-                    System.out.println("Оценка должна быть от 2 до 5. Введите оценку для поиска: ");
-                    while (!scanner.hasNextInt()) {
-                        System.out.println("Введите оценку для поиска: ");
-                        scanner.next();
+                case "manual_input" -> manualInput();
+                case "random_input" -> randomInput();
+                case "file_input" -> fileInput();
+                case "sort_menu" -> sortMenu();
+                case "save" -> saveData();
+                case "find" -> {
+                    if (studentsList.isEmpty()) {
+                        System.out.println("Список студентов пуст. Сначала добавьте данные.");
+                    } else {
+                        findData();
                     }
-                    grade = scanner.nextInt();
-                    scanner.nextLine();
                 }
-
-                FindThreadClass thread = new FindThreadClass(TypeFindData.Grade, students, grade);
-                thread.startThread();
-                break;
+                case "quit" -> running = false;
+                default -> System.out.println("Неверная команда");
             }
-            case "find_gradebook": {
-                System.out.println("Введите номер зачетки для поиска (6 цифр): ");
-                String input = scanner.nextLine();
-                while (!input.matches("[1-9][0-9]{5}")) {
-                    System.out.println("Номер должен содержать 6 цифр (первая не 0). Введите номер зачетки: ");
-                    input = scanner.nextLine();
-                }
-                int number = Integer.parseInt(input);
-                FindThreadClass thread = new FindThreadClass(TypeFindData.NumberGradeBook, students, number);
-                thread.startThread();
-                break;
-            }
-            case "quit":
-                System.exit(0);
-                break;
-            default:
-                System.out.println("\nНеверная команда, попробуйте снова...\n");
-                findData();
         }
+    }
+
+    private void manualInput() {
+
+        System.out.println("Введите количество студентов:");
+        int count = readInt();
+
+        WriteFileClass writer = new WriteFileClass();
+        studentsList = writer.writeDataStudent(count);
+        System.out.println(studentsList.size());
+    }
+
+    private void randomInput() {
+
+        System.out.println("Введите количество студентов:");
+        int count = readInt();
+
+        RandomFileClass generator = new RandomFileClass();
+
+        studentsList = generator.generate(count);
+    }
+
+    private void fileInput() {
+
+        ReadFileClass readFileClass = new ReadFileClass();
+
+        System.out.println("Введите имя файла для ввода:");
+        String fileName = scanner.nextLine();
+
+        System.out.println("Введите количество студентов:");
+        int count = readInt();
+
+        studentsList = readFileClass.readFile(fileName, count);
+    }
+
+    private void sortMenu() {
+
+        while (true) {
+
+            printSortMenu();
+            String command = scanner.nextLine();
+
+            switch (command) {
+
+                case "sort_name" -> studentsList.sortByName();
+                case "sort_grade" -> studentsList.sortByGrade();
+                case "sort_gradebook" -> studentsList.sortByNumberGradebook();
+                case "back" -> {
+                    return;
+                }
+                case "quit" -> System.exit(0);
+                default -> System.out.println("Неверная команда");
+            }
+        }
+    }
+
+    private void findData() {
+
+        while (true) {
+
+            printSearchMenu();
+
+            String command = scanner.nextLine();
+
+            switch (command) {
+
+                case "find_name" -> findByName();
+                case "find_grade" -> findByGrade();
+                case "find_gradebook" -> findByGradebook();
+                case "back" -> {
+                    return;
+                }
+                case "quit" -> System.exit(0);
+                default -> System.out.println("Неверная команда");
+            }
+        }
+    }
+
+    private void findByName() {
+
+        System.out.println("Введите имя студента:");
+
+        String name = scanner.nextLine();
+
+        while (!name.matches("^[a-zA-Zа-яА-ЯёЁ]+$")) {
+            System.out.println("Неправильное имя. Введите имя студента:");
+            name = scanner.nextLine();
+        }
+
+        FindThreadClass thread =
+                new FindThreadClass(TypeFindData.Name, studentsList, name);
+
+        thread.startThread();
+    }
+
+    private void findByGrade() {
+
+        System.out.println("Введите оценку:");
+
+        int grade = readInt();
+
+        while (grade < 2 || grade > 5) {
+            System.out.println("Оценка должна быть от 2 до 5:");
+            grade = readInt();
+        }
+
+        FindThreadClass thread =
+                new FindThreadClass(TypeFindData.Grade, studentsList, grade);
+
+        thread.startThread();
+    }
+
+    private void findByGradebook() {
+
+        System.out.println("Введите номер зачётки (6 цифр):");
+
+        String input = scanner.nextLine();
+
+        while (!input.matches("[1-9][0-9]{5}")) {
+            System.out.println("Номер должен содержать 6 цифр:");
+            input = scanner.nextLine();
+        }
+
+        int number = Integer.parseInt(input);
+
+        FindThreadClass thread =
+                new FindThreadClass(TypeFindData.NumberGradeBook, studentsList, number);
+
+        thread.startThread();
+    }
+
+    private void saveData() {
+
+        System.out.println("Введите имя файла:");
+        String name = scanner.nextLine();
+        fileManager.writeDataInFile(studentsList, name);
+        studentsList.clear();
+    }
+
+    private void printMainMenu() {
+        System.out.println(
+                """
+                Меню 'Выбор ввода данных'
+                manual_input  - ввод вручную
+                random_input  - случайные данные
+                file_input    - загрузка из файла
+                sort_menu     - сортировка
+                save          - сохранение
+                find          - поиск
+                quit          - выход
+                """
+        );
+    }
+
+    private void printSortMenu() {
+        System.out.println(
+                """
+                Меню 'Сортировка данных'
+                sort_name       - сортировка по имени
+                sort_grade      - сортировка по среднему баллу
+                sort_gradebook  - сортировка по номеру зачетки
+                back            - вернуться в главное меню
+                quit            - закрыть приложение
+                """
+        );
+    }
+
+    private void printSearchMenu() {
+        System.out.println(
+                """
+                Меню 'Поиск данных'
+                find_name       - поиск по имени
+                find_grade      - отсортировать по оценочному баллу
+                find_gradebook  - отсортировать по номеру зачетной книжки
+                back            - вернуться в главное меню
+                quit            - закрыть приложение
+                """
+        );
+    }
+
+    private int readInt() {
+
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+
+        int value = scanner.nextInt();
+        scanner.nextLine();
+
+        return value;
     }
 }
